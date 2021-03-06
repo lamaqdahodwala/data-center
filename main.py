8132e73cfdf1c9ae15129e54d257c4abcd7100ee
Lamaq Dahodwala <ldahodwala@wtsdnj.com>
2021-03-06 18:58:43 +0000

    update

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from flask import Flask
 import string
 import random
 import flask_cors
-import json
-from threading import Thread
-from time import sleep
 from replit import db
+
+
 app = Flask(__name__)
 
 flask_cors.CORS(app)
@@ -16,20 +15,32 @@ def home():
     return ''
 
 
-@app.route('/exists/<token>')
 def exists(token):
     if token in db.keys():
-        return 'true'
+        return True
     else:
-        return 'false'
+        return False
+
+@app.route('/generate')
+def gen():
+    token = ''
+    for i in range(50):
+        letter = random.choice(list(string.ascii_letters))
+        token += letter
+    if exists(token):
+        gen()
+    else:
+        db[token] = {'points': 0}
+        return token
+
 
 
 @app.route('/grabdata/<token>')
 def grabdata(token):
-    pass
+    return str(db[token])
 
 @app.route('/updateitem/<item>/<token>/<amount>')
 def updatedata(item, token, amount):
     pass
 
-app.run('127.0.0.1', port='8080')
+app.run('0.0.0.0', port='8080')
