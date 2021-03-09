333f16c334ff56fead637b4f5beae52108da8778
Lamaq Dahodwala <74932012+lamaqdahodwala@users.noreply.github.com>
2021-03-08 19:59:24 -0500

    generation integration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tornado.ioloop
 from tornado.web import RequestHandler, Application, url
+import string
+import random
 from replit import db
 
 
@@ -25,10 +27,32 @@ class UpdateHandler(RequestHandler):
             self.write('err: token doesn\'t exist.')
             return
 
+class GenerationHandler(RequestHandler):
+    def set_default_headers(self):
+        self.set_header('Access-Control-Allow-Origin', '*')
+    def exists(self, token):
+        if token in db.keys():
+            return True
+        else:
+            return False
+    def get(self):
+        letters = list(string.ascii_letters)
+        tkn = ''
+        for i in range(50):
+            tkn += random.choice(letters)
+        if self.exists(tkn):
+            get()
+        else:
+            self.write(tkn)
+        
+
+
+        
 def main():
     return Application([
         url(r'/', HomeHandler),
-        url(r'/updateitem/(.+)/(.+)/(\d+)', UpdateHandler)
+        url(r'/updateitem/(.+)/(.+)/(\d+)', UpdateHandler),
+        url(r'/generate', GenerationHandler)
 
     ])
 if __name__ == '__main__':
