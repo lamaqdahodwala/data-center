33875acf00d217c855136cc7e0f1637405a9b6dc
LAMAQ DAHODWALA <ldahodwala@wtsdnj.com>
2021-03-09 16:40:59 +0000

    cors fix

--- main.py
+++ main.py
@@ -88,6 +88,8 @@ class GenerationHandler(RequestHandler):
         
 
 class GrabHandler(RequestHandler):
+    def set_default_headers(self):
+        self.set_header('Access-Control-Allow-Origin', '*')
     def get(self, token):
         if token in db.keys():
             self.write(db[token])
